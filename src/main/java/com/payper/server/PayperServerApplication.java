f16662fcacb50cb146e5f9291c50c03701b6de5b
answl <dms2107@sookmyung.ac.kr>
2026-01-03 02:51:42 +0900

    feature: 프로젝트 초기 설정

---
+++ src/main/java/com/payper/server/PayperServerApplication.java
@@ -0,0 +1,13 @@
+package com.payper.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayperServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payper/server/PayperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payper.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayperServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
