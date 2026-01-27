daa1e8d4e1967d93f23d290109f536180cbbea43
khyun9807 <khyun9807@naver.com>
2026-01-27 17:03:16 +0900

    SecurityConfig, CustomAccessDeniedHandler

--- src/main/java/com/payper/server/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/payper/server/security/CustomAccessDeniedHandler.java
@@ -8,11 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     private final ObjectMapper objectMapper;
 
