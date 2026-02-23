12ace1d416363ebc355d8d30f483c3737ecaae1d
seoyeon2001 <syeon1250@naver.com>
2026-02-23 21:26:41 +0900

    fix: formatting

--- src/test/java/com/payper/server/UserAndRefreshTokenJpaTest.java
+++ src/test/java/com/payper/server/UserAndRefreshTokenJpaTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
