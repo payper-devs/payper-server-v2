f9bc0527773bd85fd6e1f1cc70e07f4f922f96f3
seoyeon2001 <syeon1250@naver.com>
2026-02-23 20:44:50 +0900

    chore: formatting

--- src/main/java/com/payper/server/auth/dto/request/LoginRequest.java
+++ src/main/java/com/payper/server/auth/dto/request/LoginRequest.java
@@ -9,8 +9,7 @@ import lombok.Setter;
 @Setter
 @Schema(description = "로그인 요청")
 public class LoginRequest {
-    @Schema(
-            description = "OAuth Provider가 제공한 access token", example = "kakao_oauth_token_example")
+    @Schema(description = "OAuth Provider가 제공한 access token", example = "kakao_oauth_token_example")
     @NotBlank(message = "OAuth Provider가 제공한 oauth resource access token이 필요합니다.")
     private String oauthToken;
 }
